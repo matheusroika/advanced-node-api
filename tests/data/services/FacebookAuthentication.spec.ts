7598a58d965af92e9ff255d3fb72cc912974e56d
Matheus Roika <matheusroika@hotmail.com>
2023-08-07 23:08:48 -0300

    test: ensure FacebookAuthenticationService returns AuthenticationError when LoadFacebookUser returns undefined

--- tests/data/services/FacebookAuthentication.spec.ts
+++ tests/data/services/FacebookAuthentication.spec.ts
@@ -1,4 +1,5 @@
 import { FacebookAuthenticationService } from '@/data/services'
+import { AuthenticationError } from '@/domain/errors'
 
 type Sut = {
   sut: FacebookAuthenticationService
@@ -6,18 +7,21 @@ type Sut = {
 }
 
 export interface LoadFacebookUser {
-  loadUser: (params: LoadFacebookUser.Params) => Promise<void>
+  loadUser: (params: LoadFacebookUser.Params) => Promise<LoadFacebookUser.Result>
 }
 
 namespace LoadFacebookUser {
   export type Params = {
     token: string
   }
+  export type Result = undefined
 }
 
 const mockLoadFacebookUser = (): LoadFacebookUser => {
   class LoadFacebookUserStub implements LoadFacebookUser {
-    async loadUser (params: LoadFacebookUser.Params): Promise<void> {}
+    async loadUser (params: LoadFacebookUser.Params): Promise<LoadFacebookUser.Result> {
+      return undefined
+    }
   }
   return new LoadFacebookUserStub()
 }
@@ -37,5 +41,12 @@ describe('Facebook Authentication Service', () => {
     const loadUserSpy = jest.spyOn(loadFacebookUserStub, 'loadUser')
     await sut.auth({ token: 'any_token' })
     expect(loadUserSpy).toHaveBeenCalledWith({ token: 'any_token' })
+  })
+
+  test('Should return AuthenticationError when LoadFacebookUser returns undefined', async () => {
+    const { sut, loadFacebookUserStub } = makeSut()
+    jest.spyOn(loadFacebookUserStub, 'loadUser').mockResolvedValueOnce(undefined)
+    const authResult = await sut.auth({ token: 'any_token' })
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
