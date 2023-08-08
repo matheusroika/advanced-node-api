8bc08dd48cfbe499d45db83e711c2fa2f8bde624
Matheus Roika <matheusroika@hotmail.com>
2023-08-08 03:19:36 -0300

    test: ensure FacebookAccount model doesn't update name if accountData.name is truthy

--- tests/domain/models/FacebookAccount.spec.ts
+++ tests/domain/models/FacebookAccount.spec.ts
@@ -10,4 +10,9 @@ describe('Facebook Account Model', () => {
     const sut = new FacebookAccount({ name: 'Facebook Name', email: 'any@email.com', facebookId: 'any_fb_id' }, { id: 'any_id' })
     expect(sut).toEqual({ id: 'any_id', name: 'Facebook Name', email: 'any@email.com', facebookId: 'any_fb_id' })
   })
+
+  test('Should not update name if accountData.name is truthy', () => {
+    const sut = new FacebookAccount({ name: 'Facebook Name', email: 'any@email.com', facebookId: 'any_fb_id' }, { id: 'any_id', name: 'Any Name' })
+    expect(sut).toEqual({ id: 'any_id', name: 'Any Name', email: 'any@email.com', facebookId: 'any_fb_id' })
+  })
 })
