9da0cad6a8b0ce93041dc08854d03a893db78429
Matheus Roika <matheusroika@hotmail.com>
2023-08-08 00:09:07 -0300

    feat: ensure FacebookAuthenticationService calls CreateUserAccountRepository when LoadUserAccountRepository returns undefined

--- src/data/contracts/repositories/userAccount.ts
+++ src/data/contracts/repositories/userAccount.ts
@@ -1,9 +1,22 @@
 export interface LoadUserAccountRepository {
-  load: (params: LoadUserAccountRepository.Params) => Promise<void>
+  load: (params: LoadUserAccountRepository.Params) => Promise<LoadUserAccountRepository.Result>
 }
 
 export namespace LoadUserAccountRepository {
   export type Params = {
     email: string
+  }
+  export type Result = undefined
+}
+
+export interface CreateUserAccountFromFacebookRepository {
+  createFromFacebook: (params: CreateUserAccountFromFacebookRepository.Params) => Promise<void>
+}
+
+export namespace CreateUserAccountFromFacebookRepository {
+  export type Params = {
+    email: string
+    name: string
+    facebookId: string
   }
 }
--- src/data/services/FacebookAuthentication.ts
+++ src/data/services/FacebookAuthentication.ts
@@ -1,20 +1,21 @@
 import { AuthenticationError } from '@/domain/errors'
 import type { FacebookAuthentication } from '@/domain/features'
 import type { LoadFacebookUser } from '@/data/contracts/apis'
-import type { LoadUserAccountRepository } from '@/data/contracts/repositories'
+import type { CreateUserAccountFromFacebookRepository, LoadUserAccountRepository } from '@/data/contracts/repositories'
 
 export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor (
     private readonly loadFacebookUser: LoadFacebookUser,
-    private readonly loadUserAccountRepository: LoadUserAccountRepository
+    private readonly loadUserAccountRepository: LoadUserAccountRepository,
+    private readonly createUserAccountFromFacebookRepository: CreateUserAccountFromFacebookRepository
   ) {}
 
   async auth (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
     const { token } = params
     const user = await this.loadFacebookUser.loadUser({ token })
     if (!user) return new AuthenticationError()
-    const { email } = user
-    await this.loadUserAccountRepository.load({ email })
+    await this.loadUserAccountRepository.load({ email: user.email })
+    await this.createUserAccountFromFacebookRepository.createFromFacebook(user)
     return ''
   }
 }
