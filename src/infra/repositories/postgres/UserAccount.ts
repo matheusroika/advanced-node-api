6885f13d4c9f609270c6b2d9025e8a1f4eccf41b
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 02:36:22 -0300

    feat: ensure PostgresUserAccountRepository returns an account if email exists

---
+++ src/infra/repositories/postgres/UserAccount.ts
@@ -0,0 +1,14 @@
+import type { LoadUserAccountRepository } from '@/data/contracts/repositories'
+import { PostgresUser } from '@/tests/infra/repositories/postgres/UserAccount.spec'
+
+export class PostgresUserAccountRepository implements LoadUserAccountRepository {
+  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
+    const { email } = params
+    const user = await PostgresUser.findOne({ where: { email } })
+    if (!user) return undefined
+    return {
+      id: user.id.toString(),
+      name: user.name ?? undefined
+    }
+  }
+}
