c7501388ca318ce38d642b4f84dec730aeb08d67
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 00:17:32 -0300

    test: ensure Facebook api calls httpClient.get with correct params to get User Data

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -12,7 +12,9 @@ const clientSecret = 'any_client_secret'
 
 const makeSut = (): Sut => {
   const httpClient = mock<HttpGetClient>()
-  httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+  httpClient.get
+    .mockResolvedValueOnce({ access_token: 'any_app_token' })
+    .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
   const sut = new FacebookApi(httpClient, clientId, clientSecret)
   return {
     sut,
@@ -42,6 +44,18 @@ describe('Facebook Api', () => {
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  })
+
+  test('Should call httpClient.get with correct params to get User Data', async () => {
+    const { sut, httpClient } = makeSut()
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   })
