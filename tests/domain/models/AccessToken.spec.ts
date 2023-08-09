18e6d09a46e317d333d3e5db097a0a672d82b15d
Matheus Roika <matheusroika@hotmail.com>
2023-08-08 22:10:24 -0300

    test: ensure AccessToken model expires after 30 minutes

--- tests/domain/models/AccessToken.spec.ts
+++ tests/domain/models/AccessToken.spec.ts
@@ -1,8 +1,12 @@
 import { AccessToken } from '@/domain/models'
 
 describe('Access Token Model', () => {
-  test('Should create with a value', () => {
+  test('Should create instance with a value', () => {
     const sut = new AccessToken('any_value')
     expect(sut).toEqual({ value: 'any_value' })
+  })
+
+  test('Should expire after 30 minutes', () => {
+    expect(AccessToken.validTimeInMs).toBe(1800000)
   })
 })
