b0c53933d74352fe8c2b4d03875ea4b0fe69f3e1
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 00:13:08 -0300

    feat: ensure Facebook api calls httpClient.get with correct params to get Debug Token

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -11,12 +11,22 @@ export class FacebookApi {
   ) {}
 
   async loadUser (params: LoadFacebookUser.Params): Promise<void> {
-    await this.httpClient.get({
+    const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
         client_secret: this.clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+
+    console.log(appToken)
+
+    await this.httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
+      params: {
+        access_token: appToken.access_token,
+        input_token: params.token
       }
     })
   }
