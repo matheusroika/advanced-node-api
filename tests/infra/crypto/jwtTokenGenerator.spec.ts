d4977bbb3802a1c98231a2413cf5645e1bd39c9a
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 01:49:46 -0300

    test: ensure JwtTokenGenerator returns a token on jwt.sign success

--- tests/infra/crypto/jwtTokenGenerator.spec.ts
+++ tests/infra/crypto/jwtTokenGenerator.spec.ts
@@ -11,6 +11,7 @@ type Sut = {
 const makeSut = (): Sut => {
   const sut = new JwtTokenGenerator('any_secret')
   const fakeJwt = jwt as jest.Mocked<typeof jwt>
+  fakeJwt.sign.mockImplementation(() => 'any_token')
   return {
     sut,
     fakeJwt
@@ -22,5 +23,11 @@ describe('JWT Token Generator', () => {
     const { sut, fakeJwt } = makeSut()
     await sut.generateToken({ key: 'any_key', validTimeInMs: 1000 })
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1000 })
+  })
+
+  test('Should return a token on jwt.sign success', async () => {
+    const { sut } = makeSut()
+    const token = await sut.generateToken({ key: 'any_key', validTimeInMs: 1000 })
+    expect(token).toBe('any_token')
   })
 })
