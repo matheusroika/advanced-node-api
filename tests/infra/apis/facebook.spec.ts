78c659cb591607624f044d28a9ac45d53733eb65
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 00:13:28 -0300

    test: ensure Facebook api calls httpClient.get with correct params to get Debug Token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -12,6 +12,7 @@ const clientSecret = 'any_client_secret'
 
 const makeSut = (): Sut => {
   const httpClient = mock<HttpGetClient>()
+  httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
   const sut = new FacebookApi(httpClient, clientId, clientSecret)
   return {
     sut,
@@ -20,7 +21,7 @@ const makeSut = (): Sut => {
 }
 
 describe('Facebook Api', () => {
-  test('Should call httpClient.get with correct params', async () => {
+  test('Should call httpClient.get with correct params to get App Token', async () => {
     const { sut, httpClient } = makeSut()
     await sut.loadUser({ token: 'any_client_token' })
     expect(httpClient.get).toHaveBeenCalledWith({
@@ -29,6 +30,18 @@ describe('Facebook Api', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  test('Should call httpClient.get with correct params to get Debug Token', async () => {
+    const { sut, httpClient } = makeSut()
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
