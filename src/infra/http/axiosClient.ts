3bb5025338744ef2e127e8d4ea805b8f1517ed9e
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 01:34:45 -0300

    fix: add implementation of HttpGetClient to AxiosClient

--- src/infra/http/axiosClient.ts
+++ src/infra/http/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { HttpGetClient } from '@/infra/http'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get <Result = any> (args: HttpGetClient.Params): Promise<Result> {
     const { url, params } = args
     const result = await axios.get(url, { params })
