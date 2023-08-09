85bae597b2cb0513e58a734fa4342090a63f889b
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 03:25:48 -0300

    feat: ensure PostgresUserAccountRepository creates an account if id is undefined

--- src/infra/postgres/repositories/UserAccount.ts
+++ src/infra/postgres/repositories/UserAccount.ts
@@ -1,7 +1,7 @@
-import type { LoadUserAccountRepository } from '@/data/contracts/repositories'
+import type { LoadUserAccountRepository, SaveFacebookUserAccountRepository } from '@/data/contracts/repositories'
 import { PostgresUser } from '@/infra/postgres/entities'
 
-export class PostgresUserAccountRepository implements LoadUserAccountRepository {
+export class PostgresUserAccountRepository implements LoadUserAccountRepository, SaveFacebookUserAccountRepository {
   async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
     const { email } = params
     const user = await PostgresUser.findOne({ where: { email } })
@@ -10,5 +10,11 @@ export class PostgresUserAccountRepository implements LoadUserAccountRepository 
       id: user.id.toString(),
       name: user.name ?? undefined
     }
+  }
+
+  async saveWithFacebook (params: SaveFacebookUserAccountRepository.Params): Promise<SaveFacebookUserAccountRepository.Result> {
+    const { email, name, facebookId } = params
+    await PostgresUser.save({ email, name, facebookId })
+    return { id: '' }
   }
 }
