6cda36077adf539cbb9338488edb0b564c55ab3a
Matheus Roika <matheusroika@hotmail.com>
2023-08-10 00:14:43 -0300

    test: ensure Validation Composite returns only the first Error if multiple Validators returns Error

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,4 +1,4 @@
-import { type MockProxy, mock } from 'jest-mock-extended'
+import { mock } from 'jest-mock-extended'
 
 interface Validator {
   validate: () => Error | undefined
@@ -10,29 +10,29 @@ class ValidationComposite implements Validator {
   ) {}
 
   validate (): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error) return error
+    }
     return undefined
-  }
-}
-
-type Sut = {
-  sut: ValidationComposite
-  validators: Array<MockProxy<Validator>>
-}
-
-const makeSut = (): Sut => {
-  const validators = [mock<Validator>(), mock<Validator>()]
-  validators.forEach(validator => { validator.validate.mockReturnValueOnce(undefined) })
-  const sut = new ValidationComposite(validators)
-  return {
-    sut,
-    validators
   }
 }
 
 describe('Validation Composite', () => {
   test('Should return undefined if all Validators returns undefined', () => {
-    const { sut } = makeSut()
+    const validators = [mock<Validator>(), mock<Validator>()]
+    validators.forEach(validator => { validator.validate.mockReturnValueOnce(undefined) })
+    const sut = new ValidationComposite(validators)
     const error = sut.validate()
     expect(error).toBeUndefined()
+  })
+
+  test('Should return only the first Error if multiple Validators returns Error', () => {
+    const validators = [mock<Validator>(), mock<Validator>()]
+    validators[0].validate.mockReturnValueOnce(new Error('error 1'))
+    validators[1].validate.mockReturnValueOnce(new Error('error 2'))
+    const sut = new ValidationComposite(validators)
+    const error = sut.validate()
+    expect(error).toEqual(new Error('error 1'))
   })
 })
