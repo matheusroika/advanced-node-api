5df969ec0123b8ef0d894190ad61d3b355ae0eed
Matheus Roika <matheusroika@hotmail.com>
2023-08-09 23:25:01 -0300

    test: ensure RequiredStringValidator returns undefined if value is not empty

--- tests/application/validation/requiredString.spec.ts
+++ tests/application/validation/requiredString.spec.ts
@@ -1,32 +1,39 @@
 import { RequiredFieldError } from '@/application/errors'
 
-class RequiredFieldValidator {
+class RequiredStringValidator {
   constructor (
     private readonly value: string | undefined | null,
     private readonly fieldName: string
   ) {}
 
   validate (): Error | undefined {
-    return new RequiredFieldError('any_field')
+    if (this.value) return
+    return new RequiredFieldError(this.fieldName)
   }
 }
 
 describe('Required Field Validator', () => {
   test('Should return RequireFieldError if value is empty', () => {
-    const sut = new RequiredFieldValidator('', 'any_field')
+    const sut = new RequiredStringValidator('', 'any_field')
     const error = sut.validate()
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
 
   test('Should return RequireFieldError if value is undefined', () => {
-    const sut = new RequiredFieldValidator(undefined, 'any_field')
+    const sut = new RequiredStringValidator(undefined, 'other_field')
     const error = sut.validate()
-    expect(error).toEqual(new RequiredFieldError('any_field'))
+    expect(error).toEqual(new RequiredFieldError('other_field'))
   })
 
   test('Should return RequireFieldError if value is null', () => {
-    const sut = new RequiredFieldValidator(null, 'any_field')
+    const sut = new RequiredStringValidator(null, 'third_field')
     const error = sut.validate()
-    expect(error).toEqual(new RequiredFieldError('any_field'))
+    expect(error).toEqual(new RequiredFieldError('third_field'))
+  })
+
+  test('Should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidator('any_value', 'any_field')
+    const error = sut.validate()
+    expect(error).toEqual(undefined)
   })
 })
