7ac5421a40f5bdb7ed36294e06aa074294b835f9
Matheus Roika <matheusroika@hotmail.com>
2023-08-10 03:04:42 -0300

    test: ensure External Facebook API returns a Facebook User if token is valid

---
+++ tests/external/facebookApi.test.ts
@@ -0,0 +1,28 @@
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+
+type Sut = {
+  sut: FacebookApi
+}
+
+const makeSut = (): Sut => {
+  const axiosClient = new AxiosHttpClient()
+  const sut = new FacebookApi(
+    axiosClient, process.env.FACEBOOK_CLIENT_ID as string, process.env.FACEBOOK_CLIENT_SECRET as string
+  )
+  return {
+    sut
+  }
+}
+
+describe('Facebook External API', () => {
+  test('Should return a Facebook User if token is valid', async () => {
+    const { sut } = makeSut()
+    const fbUser = await sut.loadUser({ token: process.env.FACEBOOK_USER_TEST_TOKEN as string })
+    expect(fbUser).toEqual({
+      facebookId: process.env.FACEBOOK_USER_TEST_ID as string,
+      email: process.env.FACEBOOK_USER_TEST_EMAIL as string,
+      name: process.env.FACEBOOK_USER_TEST_NAME as string
+    })
+  })
+})
