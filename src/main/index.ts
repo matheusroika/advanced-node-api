82710a605a06e9c2cdf49da8f2ea290329f2cecc
Matheus Roika <matheusroika@hotmail.com>
2023-08-11 01:45:27 -0300

    feat: add database connection to index file

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,12 @@
 import 'dotenv/config'
 import 'reflect-metadata'
 import app from '@/main/config/app'
+import { appDataSource } from '@/main/config/db'
 
 const port = process.env.PORT ?? 8080
-app.listen(port, () => { console.log(`Server running in port ${port}`) })
+appDataSource.initialize()
+  .then(async () => {
+    await appDataSource.synchronize()
+    app.listen(port, () => { console.log(`Server running in port ${port}`) })
+  })
+  .catch(console.error)
