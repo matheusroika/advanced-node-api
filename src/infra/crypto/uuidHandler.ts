e05e6d9def186274405d16d2ee573ae2748eb11a
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 14:58:39 -0300

    feat: ensure uuidHandler returns correct uuid

--- src/infra/crypto/uuidHandler.ts
+++ src/infra/crypto/uuidHandler.ts
@@ -1,9 +1,9 @@
-import { v4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 import type { UUIDGenerator } from '@/domain/contracts/crypto'
 
 export class UUIDHandler implements UUIDGenerator {
-  uuid (params: UUIDGenerator.Params): UUIDGenerator.Result {
-    v4()
-    return ''
+  uuid ({ key }: UUIDGenerator.Params): UUIDGenerator.Result {
+    const uuid = `${key}_${uuidv4()}`
+    return uuid
   }
 }
