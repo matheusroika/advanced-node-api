c4aaaf72dd9940a36f1a71779ecec8cca5e2fe4b
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 01:55:50 -0300

    feat: ensure ChangeProfilePicture use case calls UploadFile with correct params

--- src/domain/useCases/changeProfilePicture.ts
+++ src/domain/useCases/changeProfilePicture.ts
@@ -1,12 +1,13 @@
 import type { ChangeProfilePicture } from '@/domain/features'
-import type { UploadFile } from '@/tests/domain/useCases/changeProfilePicture.spec'
+import type { UUIDGenerator, UploadFile } from '@/tests/domain/useCases/changeProfilePicture.spec'
 
 export class ChangeProfilePictureUseCase implements ChangeProfilePicture {
   constructor (
-    private readonly fileStorage: UploadFile
+    private readonly fileStorage: UploadFile,
+    private readonly crypto: UUIDGenerator
   ) {}
 
   async change ({ userId, file }: ChangeProfilePicture.Params): Promise<void> {
-    await this.fileStorage.upload({ file, key: userId })
+    await this.fileStorage.upload({ file, key: this.crypto.uuid({ key: userId }) })
   }
 }
