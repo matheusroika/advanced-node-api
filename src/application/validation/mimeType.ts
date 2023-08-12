9ac5d70932a65fecdff6cb428f07a1d1074197ea
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 20:53:05 -0300

    feat: ensure MimeTypeValidator returns undefined if value is valid

--- src/application/validation/mimeType.ts
+++ src/application/validation/mimeType.ts
@@ -1,12 +1,21 @@
 import { InvalidMimeTypeError } from '@/application/errors'
+import type { Validator } from '.'
 
-export class MimeTypeValidator {
+enum AllowedExtensions {
+  png = 'image/png',
+  jpg = 'image/jpeg',
+  jpeg = 'image/jpeg'
+}
+
+export class MimeTypeValidator implements Validator {
   constructor (
-    private readonly allowed: string[],
+    private readonly allowed: Array<keyof typeof AllowedExtensions>,
     private readonly mimeType: string
   ) {}
 
-  validate (): Error {
-    return new InvalidMimeTypeError(this.allowed)
+  validate (): Error | undefined {
+    for (const extension of this.allowed) {
+      if (AllowedExtensions[extension] !== this.mimeType) return new InvalidMimeTypeError(this.allowed)
+    }
   }
 }
