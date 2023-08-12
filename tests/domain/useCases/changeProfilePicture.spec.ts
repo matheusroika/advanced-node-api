50a7188617f41e90c7e0444bb3efa3e9f2b92396
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 02:16:43 -0300

    test: ensure ChangeProfilePicture use case calls SaveUserPicture with correct params

--- tests/domain/useCases/changeProfilePicture.spec.ts
+++ tests/domain/useCases/changeProfilePicture.spec.ts
@@ -2,31 +2,51 @@ import { ChangeProfilePictureUseCase } from '@/domain/useCases'
 import { type MockProxy, mock } from 'jest-mock-extended'
 import type { UploadFile } from '@/domain/contracts/gateways'
 import type { UUIDGenerator } from '@/domain/contracts/crypto'
+import type { SaveUserPicture } from '@/domain/contracts/repositories'
 
 type Sut = {
   sut: ChangeProfilePictureUseCase
   fileStorage: MockProxy<UploadFile>
   crypto: MockProxy<UUIDGenerator>
+  userProfileRepository: MockProxy<SaveUserPicture>
 }
 
 const makeSut = (): Sut => {
   const fileStorage = mock<UploadFile>()
   const crypto = mock<UUIDGenerator>()
+  const userProfileRepository = mock<SaveUserPicture>()
+  fileStorage.upload.mockResolvedValue('any_url')
   crypto.uuid.mockReturnValue('any_unique_id')
-  const sut = new ChangeProfilePictureUseCase(fileStorage, crypto)
+  const sut = new ChangeProfilePictureUseCase(fileStorage, crypto, userProfileRepository)
   return {
     sut,
     fileStorage,
-    crypto
+    crypto,
+    userProfileRepository
   }
 }
 
 describe('Change Profile Picture Use Case', () => {
-  test('Should call UploadFile with correct params', async () => {
-    const { sut, fileStorage } = makeSut()
+  test('Should call UploadFile and UUIDGenerator with correct params', async () => {
+    const { sut, fileStorage, crypto } = makeSut()
     const file = Buffer.from('any_buffer')
     await sut.change({ userId: 'any_id', file })
     expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: 'any_unique_id' })
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
+    expect(crypto.uuid).toHaveBeenCalledWith({ key: 'any_id' })
+    expect(crypto.uuid).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should not call UploadFile when file is undefined', async () => {
+    const { sut, fileStorage } = makeSut()
+    await sut.change({ userId: 'any_id', file: undefined as any })
+    expect(fileStorage.upload).not.toHaveBeenCalled()
+  })
+
+  test('Should call SaveUserPicture with correct params', async () => {
+    const { sut, userProfileRepository } = makeSut()
+    await sut.change({ userId: 'any_id', file: Buffer.from('any_buffer') })
+    expect(userProfileRepository.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url' })
+    expect(userProfileRepository.savePicture).toHaveBeenCalledTimes(1)
   })
 })
