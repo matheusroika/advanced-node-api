d90d4379c8683250564d64e755b9126e9e7f3596
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 19:25:30 -0300

    test: ensure SaveProfilePicture controller returns 400 if file is not provided

---
+++ tests/application/controllers/saveProfilePicture.spec.ts
@@ -0,0 +1,33 @@
+import { SaveProfilePictureController } from '@/application/controllers'
+import { RequiredFieldError } from '@/application/errors'
+
+type Sut = {
+  sut: SaveProfilePictureController
+}
+
+const makeSut = (): Sut => {
+  const sut = new SaveProfilePictureController()
+  return {
+    sut
+  }
+}
+
+describe('Save Profile Picture Controller', () => {
+  test('Should return 400 if file is not provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({ file: undefined })
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('image')
+    })
+  })
+
+  test('Should return 400 if file is not provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({ file: null })
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('image')
+    })
+  })
+})
