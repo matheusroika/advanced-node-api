032214a040af6aad9ab0ecc249aa9c0b585dc078
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 17:54:44 -0300

    test: ensure PostgresUserProfileRepository updates user profile

---
+++ tests/infra/postgres/repositories/UserProfile.spec.ts
@@ -0,0 +1,57 @@
+import { type IBackup, type IMemoryDb } from 'pg-mem'
+import type { DataSource } from 'typeorm'
+import { PostgresUserProfileRepository } from '@/infra/postgres/repositories'
+import { PostgresUser } from '@/infra/postgres/entities'
+import { mockDb } from '@/tests/infra/postgres/helpers'
+
+type Sut = {
+  sut: PostgresUserProfileRepository
+}
+
+const makeSut = (): Sut => {
+  const sut = new PostgresUserProfileRepository()
+  return {
+    sut
+  }
+}
+
+describe('Postgres User Profile Repository', () => {
+  let dataSource: DataSource
+  let db: IMemoryDb
+  let backup: IBackup
+
+  beforeAll(async () => {
+    const mockedDb = await mockDb([PostgresUser])
+    db = mockedDb.db
+    dataSource = mockedDb.dataSource
+    backup = db.backup()
+  })
+
+  afterAll(async () => {
+    await dataSource.destroy()
+  })
+
+  afterEach(() => {
+    backup.restore()
+  })
+
+  describe('SaveUserPicture', () => {
+    test('Should update user profile', async () => {
+      const { sut } = makeSut()
+      const { id } = await PostgresUser.save({ email: 'any@email.com', initials: 'TN' })
+      await sut.savePicture({ id: id.toString(), pictureUrl: 'any_url' })
+      const user = await PostgresUser.findOne({ where: { id } })
+      expect(user?.pictureUrl).toBe('any_url')
+      expect(user?.initials).toBeNull()
+    })
+
+    test('Should update user profile', async () => {
+      const { sut } = makeSut()
+      const { id } = await PostgresUser.save({ email: 'any@email.com', pictureUrl: 'any_url' })
+      await sut.savePicture({ id: id.toString(), initials: 'TN' })
+      const user = await PostgresUser.findOne({ where: { id } })
+      expect(user?.pictureUrl).toBeNull()
+      expect(user?.initials).toBe('TN')
+    })
+  })
+})
