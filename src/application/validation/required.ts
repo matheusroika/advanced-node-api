dc172a9e9f5a20045c972a11b21b3aa0a65cf5b5
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 20:30:55 -0300

    feat: add RequiredBufferValidator

--- src/application/validation/required.ts
+++ src/application/validation/required.ts
@@ -3,12 +3,24 @@ import type { Validator } from '.'
 
 export class RequiredValidator implements Validator {
   constructor (
-    private readonly value: string | undefined | null,
-    private readonly fieldName: string
+    readonly value: any | undefined | null,
+    readonly fieldName: string
   ) {}
 
   validate (): Error | undefined {
-    if (this.value) return
-    return new RequiredFieldError(this.fieldName)
+    if (!this.value) return new RequiredFieldError(this.fieldName)
+  }
+}
+
+export class RequiredBufferValidator extends RequiredValidator {
+  constructor (
+    override readonly value: Buffer,
+    override readonly fieldName: string
+  ) {
+    super(value, fieldName)
+  }
+
+  validate (): Error | undefined {
+    if (super.validate() ?? this.value.length <= 0) return new RequiredFieldError(this.fieldName)
   }
 }
