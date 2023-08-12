2f0b0dd5be9402289e577c50f2f829e921c49463
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 03:00:48 -0300

    test: ensure ChangeProfilePicture use case calls SaveUserPicture with correct initials when name has only one letter

--- tests/domain/useCases/changeProfilePicture.spec.ts
+++ tests/domain/useCases/changeProfilePicture.spec.ts
@@ -74,6 +74,14 @@ describe('Change Profile Picture Use Case', () => {
     expect(userProfileRepository.savePicture).toHaveBeenCalledTimes(1)
   })
 
+  test('Should call SaveUserPicture with correct initials when name has only one letter', async () => {
+    const { sut, userProfileRepository } = makeSut()
+    userProfileRepository.load.mockResolvedValueOnce({ name: 't' })
+    await sut.change({ userId: 'any_id', file: undefined as any })
+    expect(userProfileRepository.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined, initials: 'T' })
+    expect(userProfileRepository.savePicture).toHaveBeenCalledTimes(1)
+  })
+
   test('Should call LoadUserProfile with correct params', async () => {
     const { sut, userProfileRepository } = makeSut()
     await sut.change({ userId: 'any_id', file: undefined as any })
