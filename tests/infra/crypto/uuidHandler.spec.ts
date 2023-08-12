ec11de0db3de63b02e94333e3f983a302e0a683c
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 14:59:09 -0300

    test: ensure uuidHandler returns correct uuid

--- tests/infra/crypto/uuidHandler.spec.ts
+++ tests/infra/crypto/uuidHandler.spec.ts
@@ -1,4 +1,5 @@
-import { v4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
+import { mocked } from 'jest-mock'
 import { UUIDHandler } from '@/infra/crypto'
 
 jest.mock('uuid')
@@ -7,6 +8,13 @@ describe('UUID Handler', () => {
   test('Should call uuid.v4', () => {
     const sut = new UUIDHandler()
     sut.uuid({ key: 'any_key' })
-    expect(v4).toHaveBeenCalledTimes(1)
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should return correct uuid', () => {
+    mocked(uuidv4).mockReturnValueOnce('any_uuid')
+    const sut = new UUIDHandler()
+    const uuid = sut.uuid({ key: 'any_key' })
+    expect(uuid).toBe('any_key_any_uuid')
   })
 })
