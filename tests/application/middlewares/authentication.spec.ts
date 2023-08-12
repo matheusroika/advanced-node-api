b65a94d942d33525fa2339b908b95e5cc48c6f63
Matheus Roika <matheusroika@hotmail.com>
2023-08-11 21:45:50 -0300

    test: ensure Authentication middleware calls Authorize with correct params

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,17 +1,38 @@
 import { ForbiddenError } from '@/application/errors'
 import { type HttpResponse, forbidden } from '@/application/helpers'
+import type { AuthorizeUseCase } from '@/domain/useCases'
+import { type MockProxy, mock } from 'jest-mock-extended'
+import { RequiredStringValidator } from '@/application/validation'
 
 type HttpRequest = { authorization: string }
 
 class AuthenticationMiddleware {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
-    return forbidden()
+  constructor (private readonly authorize: AuthorizeUseCase) {}
+
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    if (error) return forbidden()
+    await this.authorize.auth({ token: authorization })
+  }
+}
+
+type Sut = {
+  sut: AuthenticationMiddleware
+  authorize: MockProxy<AuthorizeUseCase>
+}
+
+const makeSut = (): Sut => {
+  const authorize = mock<AuthorizeUseCase>()
+  const sut = new AuthenticationMiddleware(authorize)
+  return {
+    sut,
+    authorize
   }
 }
 
 describe('Authentication Middleware', () => {
   test('Should return 403 if Authorization header is empty', async () => {
-    const sut = new AuthenticationMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({ authorization: '' })
     expect(httpResponse).toEqual({
       statusCode: 403,
@@ -20,7 +41,7 @@ describe('Authentication Middleware', () => {
   })
 
   test('Should return 403 if Authorization header is null', async () => {
-    const sut = new AuthenticationMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({ authorization: null as any })
     expect(httpResponse).toEqual({
       statusCode: 403,
@@ -29,11 +50,18 @@ describe('Authentication Middleware', () => {
   })
 
   test('Should return 403 if Authorization header is undefined', async () => {
-    const sut = new AuthenticationMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({ authorization: undefined as any })
     expect(httpResponse).toEqual({
       statusCode: 403,
       data: new ForbiddenError()
     })
+  })
+
+  test('Should call Authorize with correct params', async () => {
+    const { sut, authorize } = makeSut()
+    await sut.handle({ authorization: 'any_auth_token' })
+    expect(authorize.auth).toHaveBeenCalledWith({ token: 'any_auth_token' })
+    expect(authorize.auth).toHaveBeenCalledTimes(1)
   })
 })
