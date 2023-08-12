728131afa04e0e8f5173d4f482752b287d609632
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 02:58:45 -0300

    test: ensure ChangeProfilePicture use case calls SaveUserPicture with correct initials when user has no surname

--- tests/domain/useCases/changeProfilePicture.spec.ts
+++ tests/domain/useCases/changeProfilePicture.spec.ts
@@ -66,6 +66,14 @@ describe('Change Profile Picture Use Case', () => {
     expect(userProfileRepository.savePicture).toHaveBeenCalledTimes(1)
   })
 
+  test('Should call SaveUserPicture with correct initials when user has no surname', async () => {
+    const { sut, userProfileRepository } = makeSut()
+    userProfileRepository.load.mockResolvedValueOnce({ name: 'test' })
+    await sut.change({ userId: 'any_id', file: undefined as any })
+    expect(userProfileRepository.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined, initials: 'TE' })
+    expect(userProfileRepository.savePicture).toHaveBeenCalledTimes(1)
+  })
+
   test('Should call LoadUserProfile with correct params', async () => {
     const { sut, userProfileRepository } = makeSut()
     await sut.change({ userId: 'any_id', file: undefined as any })
