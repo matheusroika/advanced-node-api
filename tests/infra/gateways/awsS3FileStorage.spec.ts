edfa8bc1a7cf1d37ba1e9a0f785727a94f7a7099
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 16:28:42 -0300

    test: ensure awsS3FileStorage calls PutObjectCommand with correct params

--- tests/infra/gateways/awsS3FileStorage.spec.ts
+++ tests/infra/gateways/awsS3FileStorage.spec.ts
@@ -1,14 +1,18 @@
-import { S3Client } from '@aws-sdk/client-s3'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { mocked } from 'jest-mock'
 import { AwsS3FileStorage } from '@/infra/gateways'
 
 jest.mock('@aws-sdk/client-s3')
 
 describe('AWS S3 File Storage', () => {
+  const accessKey = 'any_access_key'
+  const secret = 'any_secret'
+  const bucket = 'any_bucket'
+  const key = 'any_key'
+  const file = Buffer.from('any_buffer')
+
   test('Should configure AWS credentials on creation', () => {
-    const accessKey = 'any_access_key'
-    const secret = 'any_secret'
-    const sut = new AwsS3FileStorage(accessKey, secret)
+    const sut = new AwsS3FileStorage(accessKey, secret, bucket)
     const S3ClientInstance = mocked(S3Client).mock.instances[0]
     expect(sut.client).toEqual(S3ClientInstance)
     expect(S3Client).toHaveBeenCalledTimes(1)
@@ -17,6 +21,18 @@ describe('AWS S3 File Storage', () => {
         accessKeyId: accessKey,
         secretAccessKey: secret
       }
+    })
+  })
+
+  test('Should call PutObjectCommand with correct params', async () => {
+    const sut = new AwsS3FileStorage(accessKey, secret, bucket)
+    await sut.upload({ key, file })
+    expect(PutObjectCommand).toHaveBeenCalledTimes(1)
+    expect(PutObjectCommand).toHaveBeenCalledWith({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
     })
   })
 })
