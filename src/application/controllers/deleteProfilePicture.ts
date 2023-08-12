3f280623c5189316b9f2558940ddb4f22e6165a7
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 19:17:52 -0300

    feat: ensure DeleteProfilePicture controller extends Controller

--- src/application/controllers/deleteProfilePicture.ts
+++ src/application/controllers/deleteProfilePicture.ts
@@ -1,14 +1,17 @@
 import type { ChangeProfilePicture } from '@/domain/features'
 import { type HttpResponse, noContent } from '@/application/helpers'
+import { Controller } from '.'
 
 type HttpRequest = { userId: string }
 
-export class DeleteProfilePictureController {
+export class DeleteProfilePictureController extends Controller {
   constructor (
     private readonly changeProfilePicture: ChangeProfilePicture
-  ) {}
+  ) {
+    super()
+  }
 
-  async handle ({ userId }: HttpRequest): Promise<HttpResponse<undefined>> {
+  async control ({ userId }: HttpRequest): Promise<HttpResponse<undefined>> {
     await this.changeProfilePicture.change({ userId, file: undefined })
     return noContent()
   }
