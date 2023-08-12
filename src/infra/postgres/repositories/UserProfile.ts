a57db1872704ddd6d7467b9b0b052482cb89c75d
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 18:38:34 -0300

    feat: ensure PostgresUserProfileRepository loads user profile

--- src/infra/postgres/repositories/UserProfile.ts
+++ src/infra/postgres/repositories/UserProfile.ts
@@ -1,9 +1,14 @@
 import { PostgresUser } from '@/infra/postgres/entities'
-import type { SaveUserPicture } from '@/domain/contracts/repositories'
+import type { LoadUserProfile, SaveUserPicture } from '@/domain/contracts/repositories'
 
-export class PostgresUserProfileRepository implements SaveUserPicture {
+export class PostgresUserProfileRepository implements SaveUserPicture, LoadUserProfile {
   async savePicture ({ id, pictureUrl, initials }: SaveUserPicture.Params): Promise<void> {
     // @ts-expect-error To set field to empty value must pass null
     await PostgresUser.update({ id: Number(id) }, { pictureUrl: pictureUrl ?? null, initials: initials ?? null })
+  }
+
+  async load ({ id }: LoadUserProfile.Params): Promise<LoadUserProfile.Result> {
+    const user = await PostgresUser.findOne({ where: { id: Number(id) } })
+    return user ? { name: user.name } : undefined
   }
 }
