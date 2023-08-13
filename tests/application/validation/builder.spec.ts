32363de2833edc006389909e145f953d869956bb
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 22:31:22 -0300

    test: ensure Validation builder returns new Validators

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,8 +1,17 @@
-import { RequiredValidator, ValidationBuilder } from '@/application/validation'
+import { MaxFileSizeValidator, MimeTypeValidator, RequiredBufferValidator, RequiredValidator, ValidationBuilder } from '@/application/validation'
 
 describe('Validation Builder', () => {
   test('Should return a RequiredValidator', () => {
-    const validators = ValidationBuilder.of('any_value', 'any_field').required().build()
-    expect(validators).toEqual([new RequiredValidator('any_value', 'any_field')])
+    const buffer = Buffer.from('any_buffer')
+    const validators = [
+      ...ValidationBuilder.of('any_value', 'any_field').required().mimeType().build(),
+      ...ValidationBuilder.of(buffer, 'any_field').requiredBuffer().maxFileSize(1).build()
+    ]
+    expect(validators).toEqual([
+      new RequiredValidator('any_value', 'any_field'),
+      new MimeTypeValidator('any_value'),
+      new RequiredBufferValidator(buffer, 'any_field'),
+      new MaxFileSizeValidator(1, buffer)
+    ])
   })
 })
