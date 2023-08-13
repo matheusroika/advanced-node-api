79f95a3b7defc59a6334a8127459802c5b9938d7
Matheus Roika <matheusroika@hotmail.com>
2023-08-13 19:46:47 -0300

    refactor: load .env and .env.test on tests

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,3 +1,11 @@
+import fs from 'fs'
+import path from 'path'
 import * as dotenv from 'dotenv'
 
-dotenv.config({ path: './.env.test' })
+const envFile = fs.readFileSync(path.join(__dirname + '../../.env'))
+const envTestFile = fs.readFileSync(path.join(__dirname + '../../.env.test'))
+const env = dotenv.parse(envFile)
+const envTest = dotenv.parse(envTestFile)
+const wholeEnv = { ...env, ...envTest }
+
+dotenv.populate(process.env as dotenv.DotenvPopulateInput, wholeEnv)
