9fe99580e094131a1f02404f1f5b1e42fa834689
Matheus Roika <matheusroika@hotmail.com>
2023-08-13 04:04:00 -0300

    feat: ensure multerAdapter adds req.file to req.locals if file is valid

--- src/infra/http/multerAdapter.ts
+++ src/infra/http/multerAdapter.ts
@@ -5,6 +5,8 @@ import type { RequestHandler } from 'express'
 export const adaptMulter: RequestHandler = (req, res, next) => {
   const upload = multer().single('userProfilePicture')
   upload(req, res, (error) => {
-    res.status(500).json({ error: new ServerError(error).message })
+    if (error) return res.status(500).json({ error: new ServerError(error).message })
+    if (req.file) req.locals = { ...req.locals, file: { buffer: req.file.buffer, mimeType: req.file.mimetype } }
+    next()
   })
 }
