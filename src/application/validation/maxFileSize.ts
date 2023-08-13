bbb9951a9e582d7411829d9675bb12e079f89541
Matheus Roika <matheusroika@hotmail.com>
2023-08-12 21:06:13 -0300

    feat: ensure MaxFileSizeValidator returns undefined if value is valid

--- src/application/validation/maxFileSize.ts
+++ src/application/validation/maxFileSize.ts
@@ -1,12 +1,13 @@
 import { MaxFileSizeError } from '@/application/errors'
+import type { Validator } from '.'
 
-export class MaxFileSizeValidator {
+export class MaxFileSizeValidator implements Validator {
   constructor (
     private readonly maxSizeInMb: number,
     private readonly file: Buffer
   ) {}
 
-  validate (): Error {
-    return new MaxFileSizeError(1)
+  validate (): Error | undefined {
+    if (this.file.length > this.maxSizeInMb * 1024 * 1024) return new MaxFileSizeError(1)
   }
 }
